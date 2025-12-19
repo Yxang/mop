02d8c656d49ff06eb826c1c4935a9091a74458a2
Adrian Klingen <adrian.klingen@deptagency.com>
2025-12-19 18:59:31 +0100

    Add cancel casting, remaining cast time and is casting spell

--- sim/common/mop/trinkets_phase_3_52.go
+++ sim/common/mop/trinkets_phase_3_52.go
@@ -525,6 +525,9 @@ func init() {
 			)
 			// Manually override Crit % to Crit Rating
 			statBuffAura.BuffedStatTypes = []stats.Stat{stats.CritRating}
+			statBuffAura.Aura.ApplyOnGain(func(aura *core.Aura, sim *core.Simulation) {
+				character.ReactToEvent(sim, false)
+			})
 
 			triggerAura := character.MakeProcTriggerAura(core.ProcTrigger{
 				Name: fmt.Sprintf("%s (%s)", label, versionLabel),
